0e0147a6e20a8c8391f03cef3fa79123eb95cead
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2024-01-27 05:20:59 +0530

    completed delegation sorted int list

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -1,5 +1,8 @@
 package edu.cmu.cs.cs214.rec04;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * DelegationSortedIntList -- a variant of a SortedIntList that keeps
  * count of the number of attempted element insertions (not to be confused
@@ -10,7 +13,53 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+public class DelegationSortedIntList implements IntegerList  {
+        // Write your implementation below with API documentation
+    private InheritanceSortedIntList list;
+
+    public DelegationSortedIntList() {
+        list =  new InheritanceSortedIntList();
+    }
+
+
+    public boolean add(int num) {
+        return this.list.add(num);   
+    }
+
+    public boolean addAll(IntegerList all) {
+        return this.list.addAll(all);
+    }
+
+    
+
+    public int getTotalAdded() {
+        return this.list.totalElementsAdded;
+    }
+
+    public int get(int index) {
+        return -1;
+    }
+
+    public boolean remove(int num) {
+        return false;
+    }
+
+    public boolean removeAll(IntegerList list) {
+        return false;
+    }
+
+
+    public int size() {
+        return list.size();
+    }
+
+    public String toString() {
+        for (int i = 0; i < this.list.totalElementsAdded; i++) {
+            System.out.print(list.get(i));
+            System.out.print(",");
+        }
+        return "";
+
+    }
 
 }
